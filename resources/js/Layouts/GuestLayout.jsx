d9e73b55e54e1d7f0c3a96d0c7c93b24d3985a3e
Fata Dwi Putra <dwifata28@gmail.com>
2024-10-16 09:34:17 +0700

    push landing page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,16 +1,16 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FCFAEE]">
             <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
+                {/* <Link href="/public/disnaker.ico">
+                    <ApplicationLogo className="w-10 h-10 fill-current text-[#507687]" />
+                </Link> */}
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div className="w-full sm:max-w-[60%] mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
         </div>
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,97 +1,134 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
-        remember: false,
+        email: "",
+        password: "",
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
-
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
+            <div className="flex flex-col md:flex-row w-full min-h-[80vh]">
+                {/* Left Section */}
+                <div className="w-full md:w-1/2 bg-white flex items-center justify-center p-6 sm:p-10 relative">
+                    {/* Back Button */}
+                    {/* <Link
+                        href={route("welcome")}
+                        className="text-sm text-[#B8001F] hover:text-[#507687] font-semibold absolute top-4 left-4 transition-colors duration-200 ease-in-out hover:underline"
+                    >
+                        &larr; Back
+                    </Link> */}
+                    <div className="max-w-md w-full space-y-8 mt-12">
+                        <div>
+                            <h2 className="text-center text-3xl font-bold text-[#384B70]">
+                                Selamat datang di Go-Lacak
+                            </h2>
+                            <p className="text-center text-sm text-[#507687]">
+                                Silahkan masuk ke Go-Lacak
+                            </p>
+                        </div>
+                        {status && (
+                            <div className="mb-4 font-medium text-sm text-green-600">
+                                {status}
+                            </div>
+                        )}
+                        <form onSubmit={submit} className="space-y-6">
+                            <div>
+                                <InputLabel
+                                    htmlFor="email"
+                                    value="Alamat Email"
+                                    className="text-[#507687]"
+                                />
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#B8001F] focus:border-[#B8001F] sm:text-sm"
+                                    autoComplete="username"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("email", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.email}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div>
+                                <InputLabel
+                                    htmlFor="password"
+                                    value="Kata Sandi"
+                                    className="text-[#507687]"
+                                />
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#B8001F] focus:border-[#B8001F] sm:text-sm"
+                                    autoComplete="current-password"
+                                    onChange={(e) =>
+                                        setData("password", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.password}
+                                    className="mt-2"
+                                />
+                            </div>
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
+                            <div>
+                                {/* Updated Primary Button */}
+                                <PrimaryButton
+                                    className="w-full py-2 px-6 border border-transparent text-sm font-medium rounded-full text-white bg-[#B8001F] hover:bg-[#507687] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B8001F] shadow-md transition-all duration-200 ease-in-out transform hover:scale-105 flex justify-center items-center"
+                                    disabled={processing}
+                                >
+                                    Masuk
+                                </PrimaryButton>
+                            </div>
+                            {/* <div className="flex items-center justify-between">
+                                {canResetPassword && (
+                                    <Link
+                                        href={route("register")}
+                                        className="underline text-sm text-[#507687] hover:text-[#384B70]"
+                                    >
+                                        Belum punya akun?
+                                    </Link>
+                                )}
+                            </div> */}
+                        </form>
+                    </div>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
+                {/* Right Section */}
+                <div
+                    className="hidden md:flex w-full md:w-1/2 bg-gradient-to-br from-[#507687] to-[#384B70] items-center justify-center"
+                    style={{
+                        backgroundImage: "url('semarang.jpg')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                ></div>
+            </div>
         </GuestLayout>
     );
 }
